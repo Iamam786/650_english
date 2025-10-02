827445b34d349af09a9aa7c61addffc78ef4c6b3
Abde Mustafa <abdulrahimali1998@gmail.com>
2025-10-02 19:44:31 +0530

    first

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { DocumentationAccordion } from "@/components/documentation-accordion"
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-background">
+      <DocumentationAccordion />
+    </div>
+  )
+}

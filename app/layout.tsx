f91918dcb0dfab73aed0fa534b2b6d00b11ba4f3
abdulrahimali1998@gmail.com <abdulrahimali1998@gmail.com>
2026-02-26 18:27:55 +0530

    pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,35 @@
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import { Analytics } from '@vercel/analytics/next'
-import './globals.css'
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import { Analytics } from "@vercel/analytics/next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: '650 English Phrases',
-  description: 'Created with Next.js and Tailwind CSS by AbdeMustafa',
-  generator: 'AbdeMustafa',
-}
-
+  title: "650 English Phrases",
+  description: "Created with Next.js and Tailwind CSS by AbdeMustafa",
+  generator: "AbdeMustafa",
+  manifest: "/manifest.json",
+};
+export const viewport = {
+  themeColor: "#000000",
+};
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <head >
-        <link rel="icon" href="https://img.icons8.com/emoji/48/speaking-head.png" />
+      <head>
+        <link
+          rel="icon"
+          href="https://img.icons8.com/emoji/48/speaking-head.png"
+        />
       </head>
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
         {children}
         <Analytics />
       </body>
     </html>
-  )
+  );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+import withPWAInit from "next-pwa";
 
-export default nextConfig
+const isProd =
+  process.env.VERCEL === "1" &&
+  process.env.VERCEL_ENV === "production";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: !isProd,
+});
+
+const nextConfig = {
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true },
+  reactStrictMode: true,
+};
+
+export default withPWA(nextConfig);
